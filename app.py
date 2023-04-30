eaa091c4a8df4e444ebdc231e9c24ea91ff53c08
MarkAC <56160609+MarkAC007@users.noreply.github.com>
2023-04-30 14:59:07 +0100

    .

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 import os
-import sys
-import subprocess
+import json
+from datetime import datetime
 from dotenv import load_dotenv
 from get_tide import get_tide_data, display_tide_data
 from get_weather import get_weather_data, display_weather_data
@@ -9,14 +9,29 @@ from get_weather import get_weather_data, display_weather_data
 # Load environment variables from .env file
 load_dotenv()
 
+# Load configuration from config.json file
+def load_config():
+    with open("config.json", "r") as config_file:
+        return json.load(config_file)
+
+def is_low_tide_before_10am(tide_data):
+    for event in tide_data["extremes"]:
+        event_time = datetime.fromtimestamp(event["dt"])
+        if event["type"] == "low" and event_time.hour < 10:
+            return True
+    return False
+
+def is_no_rain(weather_data):
+    return weather_data["weather"][0]["main"].lower() != "rain"
+
+def is_wind_from_east_or_west(weather_data):
+    wind_deg = weather_data["wind"]["deg"]
+    return (wind_deg > 80 and wind_deg < 100) or (wind_deg > 260 and wind_deg < 280)
 
 def main():
-    try:
-        latitude = float(input("Enter the latitude of the location: "))
-        longitude = float(input("Enter the longitude of the location: "))
-    except ValueError:
-        print("Invalid input. Please enter valid latitude and longitude values.")
-        return
+    config = load_config()
+    latitude = config["latitude"]
+    longitude = config["longitude"]
 
     weather_api_key = os.environ.get("WEATHER_API_KEY")
     tide_api_key = os.environ.get("TIDE_API_KEY")
@@ -33,6 +48,11 @@ def main():
     
     print("\nTide Information:")
     display_tide_data(tide_data)
+
+    if is_low_tide_before_10am(tide_data) and is_no_rain(weather_data) and is_wind_from_east_or_west(weather_data):
+        print("\nConditions met: Low tide before 10 am, no rain, and wind from east or west.")
+    else:
+        print("\nConditions not met.")
 
 if __name__ == "__main__":
     main()
